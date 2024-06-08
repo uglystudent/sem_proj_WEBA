a61c125f615fc27426e86db5f6d3366ce24bd8de
uglystudent <167689173+uglystudent@users.noreply.github.com>
2024-06-08 17:25:57 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import Link from "next/link";
 import dbConnect from "../lib/dbConnect";
-import Pet, { Pets } from "../models/Pet";
+import Pet from "../models/Pet";
 import { GetServerSideProps } from "next";
+import mongoose from "mongoose";
+
+type PetType = {
+  _id: string;
+  name: string;
+  owner_name: string;
+  image_url: string;
+  likes: string[];
+  dislikes: string[];
+};
 
 type Props = {
-  pets: Pets[];
+  pets: PetType[];
 };
 
 const Index = ({ pets }: Props) => {
@@ -13,7 +23,7 @@ const Index = ({ pets }: Props) => {
       {pets.map((pet) => (
         <div key={pet._id}>
           <div className="card">
-            <img src={pet.image_url} />
+            <img src={pet.image_url} alt={`${pet.name}'s image`} />
             <h5 className="pet-name">{pet.name}</h5>
             <div className="main-content">
               <p className="pet-name">{pet.name}</p>
@@ -24,7 +34,7 @@ const Index = ({ pets }: Props) => {
                 <p className="label">Likes</p>
                 <ul>
                   {pet.likes.map((data, index) => (
-                    <li key={index}>{data} </li>
+                    <li key={index}>{data}</li>
                   ))}
                 </ul>
               </div>
@@ -32,7 +42,7 @@ const Index = ({ pets }: Props) => {
                 <p className="label">Dislikes</p>
                 <ul>
                   {pet.dislikes.map((data, index) => (
-                    <li key={index}>{data} </li>
+                    <li key={index}>{data}</li>
                   ))}
                 </ul>
               </div>
@@ -58,15 +68,19 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
   await dbConnect();
 
   /* find all the data in our database */
-  const result = await Pet.find({});
+  const result = await Pet.find({}).lean();
 
-  /* Ensures all objectIds and nested objectIds are serialized as JSON data */
-  const pets = result.map((doc) => {
-    const pet = JSON.parse(JSON.stringify(doc));
-    return pet;
-  });
+  /* Ensures all objectIds are serialized as strings */
+  const pets = result.map((doc) => ({
+    _id: doc._id.toString(),
+    name: doc.name,
+    owner_name: doc.owner_name,
+    image_url: doc.image_url,
+    likes: doc.likes,
+    dislikes: doc.dislikes,
+  }));
 
-  return { props: { pets: pets } };
+  return { props: { pets } };
 };
 
 export default Index;
