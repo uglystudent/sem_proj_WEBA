87f9d28d6e0ebbc7b2055ad41b4f188911b3d65d
uglystudent <167689173+uglystudent@users.noreply.github.com>
2024-06-08 17:15:59 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,18 @@ import Link from "next/link";
 import dbConnect from "../lib/dbConnect";
 import Pet, { Pets } from "../models/Pet";
 import { GetServerSideProps } from "next";
-import { Types } from "mongoose";
-
-type PetType = {
-  _id: Types.ObjectId;
-  name: string;
-  owner_name: string;
-  image_url: string;
-  likes: string[];
-  dislikes: string[];
-};
 
 type Props = {
-  pets: PetType[];
+  pets: Pets[];
 };
 
 const Index = ({ pets }: Props) => {
   return (
     <>
       {pets.map((pet) => (
-        <div key={pet._id.toString()}>
+        <div key={pet._id}>
           <div className="card">
-            <img src={pet.image_url} alt={`${pet.name}'s image`} />
+            <img src={pet.image_url} />
             <h5 className="pet-name">{pet.name}</h5>
             <div className="main-content">
               <p className="pet-name">{pet.name}</p>
@@ -34,7 +24,7 @@ const Index = ({ pets }: Props) => {
                 <p className="label">Likes</p>
                 <ul>
                   {pet.likes.map((data, index) => (
-                    <li key={index}>{data}</li>
+                    <li key={index}>{data} </li>
                   ))}
                 </ul>
               </div>
@@ -42,16 +32,16 @@ const Index = ({ pets }: Props) => {
                 <p className="label">Dislikes</p>
                 <ul>
                   {pet.dislikes.map((data, index) => (
-                    <li key={index}>{data}</li>
+                    <li key={index}>{data} </li>
                   ))}
                 </ul>
               </div>
 
               <div className="btn-container">
-                <Link href={{ pathname: "/[id]/edit", query: { id: pet._id.toString() } }}>
+                <Link href={{ pathname: "/[id]/edit", query: { id: pet._id } }}>
                   <button className="btn edit">Edit</button>
                 </Link>
-                <Link href={{ pathname: "/[id]", query: { id: pet._id.toString() } }}>
+                <Link href={{ pathname: "/[id]", query: { id: pet._id } }}>
                   <button className="btn view">View</button>
                 </Link>
               </div>
@@ -72,12 +62,11 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
 
   /* Ensures all objectIds and nested objectIds are serialized as JSON data */
   const pets = result.map((doc) => {
-    const pet = doc.toObject();
-    pet._id = pet._id.toString();
+    const pet = JSON.parse(JSON.stringify(doc));
     return pet;
   });
 
-  return { props: { pets } };
+  return { props: { pets: pets } };
 };
 
 export default Index;
