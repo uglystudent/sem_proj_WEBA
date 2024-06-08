40ffd841f4064cadf4663d88b4061b66c63a1367
uglystudent <167689173+uglystudent@users.noreply.github.com>
2024-06-08 17:29:03 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import Link from "next/link";
-import dbConnect from "../lib/dbConnect";
-import Pet from "../models/Pet";
-import { GetServerSideProps } from "next";
-import mongoose from "mongoose";
+import { GetServerSideProps } from 'next';
+import Link from 'next/link';
+import dbConnect from '../lib/dbConnect';
+import Pet, { PetDocument } from '../models/Pet';
 
 type PetType = {
   _id: string;
@@ -17,19 +16,18 @@ type Props = {
   pets: PetType[];
 };
 
-const Index = ({ pets }: Props) => {
+const Home = ({ pets }: Props) => {
   return (
-    <>
-      {pets.map((pet) => (
-        <div key={pet._id}>
-          <div className="card">
+    <div>
+      <h1>List of Pets</h1>
+      <div className="pet-list">
+        {pets.map((pet) => (
+          <div key={pet._id} className="card">
             <img src={pet.image_url} alt={`${pet.name}'s image`} />
             <h5 className="pet-name">{pet.name}</h5>
             <div className="main-content">
               <p className="pet-name">{pet.name}</p>
               <p className="owner">Owner: {pet.owner_name}</p>
-
-              {/* Extra Pet Info: Likes and Dislikes */}
               <div className="likes info">
                 <p className="label">Likes</p>
                 <ul>
@@ -46,31 +44,26 @@ const Index = ({ pets }: Props) => {
                   ))}
                 </ul>
               </div>
-
               <div className="btn-container">
-                <Link href={{ pathname: "/[id]/edit", query: { id: pet._id } }}>
+                <Link href={`/edit/${pet._id}`}>
                   <button className="btn edit">Edit</button>
                 </Link>
-                <Link href={{ pathname: "/[id]", query: { id: pet._id } }}>
+                <Link href={`/${pet._id}`}>
                   <button className="btn view">View</button>
                 </Link>
               </div>
             </div>
           </div>
-        </div>
-      ))}
-    </>
+        ))}
+      </div>
+    </div>
   );
 };
 
-/* Retrieves pet(s) data from mongodb database */
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
   await dbConnect();
 
-  /* find all the data in our database */
   const result = await Pet.find({}).lean();
-
-  /* Ensures all objectIds are serialized as strings */
   const pets = result.map((doc) => ({
     _id: doc._id.toString(),
     name: doc.name,
@@ -83,4 +76,4 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
   return { props: { pets } };
 };
 
-export default Index;
+export default Home;
